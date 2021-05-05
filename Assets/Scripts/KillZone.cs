3126868928017cd40354afbd4120e2b3746be881
Marion-DoBr <78073720+Marion-DoBr@users.noreply.github.com>
2021-05-05 10:30:32 +0200

    Initial commit

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        //Debug.Break();
+        Debug.Log(other);
+        Destroy(other.gameObject);
+    }
+
+}
